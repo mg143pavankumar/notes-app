f6d710d9f4a06bb29534904c01dc5d48bffc7cfc
mg143pavankumar <pavansharma.mg143@gmail.com>
2021-04-24 00:25:39 +0530

    Added backend

---
+++ pages/api/notes/[id].js
@@ -0,0 +1,65 @@
+import dbConnection from "../../../utils/dbConnection";
+import Note from "../../../models/Note";
+
+dbConnection();
+
+export default async (req, res) => {
+  const {
+    query: { id },
+    method,
+  } = req;
+
+  switch (method) {
+    case "GET":
+      try {
+        const note = await Note.findById(id);
+
+        if (!note) {
+          return res
+            .status(400)
+            .json({ success: false, message: "note not found" });
+        }
+
+        res.status(200).json({ success: true, data: note });
+      } catch (error) {
+        res.status(400).json({ success: false });
+      }
+      break;
+    case "PUT":
+      try {
+        const note = await Note.findByIdAndUpdate(id, req.body, {
+          new: true,
+          runValidators: true,
+        });
+
+        if (!note) {
+          return res
+            .status(400)
+            .json({ success: false, message: "note not found" });
+        }
+
+        res.status(200).json({ success: true, data: note });
+      } catch (error) {
+        res.status(400).json({ success: false });
+      }
+      break;
+    case "DELETE":
+      try {
+        const deleteNote = await Note.deleteOne({ _id: id });
+
+        if (!deleteNote) {
+          return res
+            .status(400)
+            .json({ success: false, message: "note not deleted" });
+        }
+
+        res.status(200).json({ success: true, data: {} });
+      } catch (error) {
+        res.status(400).json({ success: false });
+      }
+      break;
+    default:
+      res.status(400).json({ success: false });
+      break;
+  }
+};
