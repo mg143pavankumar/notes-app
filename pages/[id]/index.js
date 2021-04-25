335a4ac674efb2048d7b489f9b062eb7168f027b
Mistry Pavan Kumar <68140538+mg143pavankumar@users.noreply.github.com>
2021-04-25 13:55:58 +0530

    Update index.js

--- pages/[id]/index.js
+++ pages/[id]/index.js
@@ -5,7 +5,6 @@ import Head from "next/head";
 
 const Note = ({ data }) => {
   
-  const SERVER_URL = "https://notes-app-three-kappa.vercel.app";
   const [confirm, setConfirm] = useState(false);
   const [isDeleting, setIsDeleting] = useState(false);
   const router = useRouter();
@@ -24,7 +23,7 @@ const Note = ({ data }) => {
     const noteId = router.query.id;
 
     try {
-      const deleted = await fetch(`${SERVER_URL}/api/notes/${noteId}`, {
+      const deleted = await fetch(`https://notes-app-three-kappa.vercel.app/api/notes/${noteId}`, {
         method: "DELETE",
       });
 
@@ -62,7 +61,7 @@ const Note = ({ data }) => {
 };
 
 export async function getServerSideProps({ query: { id } }) {
-  const res = await fetch(`${SERVER_URL}/api/notes/${id}`);
+  const res = await fetch(`https://notes-app-three-kappa.vercel.app/api/notes/${id}`);
   const { data } = await res.json();
 
   return {
